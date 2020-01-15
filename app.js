da45a724d673cb1b33b6ac7651c3168c3919ce69
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-01-15 11:57:36 +0200

    CSS done

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ const generateHexaColor = () => {
 
 }
 
+const flexContainer = document.querySelector('.flex-container')
 
-for (let i=0; i<5; i++) {
-    console.log(generateHexaColor())
-    const div = document.createElement('div');
-    div.style.backgroundColor = generateHexaColor();
-    div.textContent = generateHexaColor();
+for (let i = 0; i < 5; i++) {
+    const title = document.createElement('h1')
+    const div = document.createElement('div')
+    title.textContent = generateHexaColor()
+    
+    div.style.border = '1px solid #999'
+       setInterval(function () {
+      
+         let bgColor = generateHexaColor()
+         title.textContent = bgColor
+         if (i %
+         2 == 0) {
+         div.style.background = bgColor
+         } else {
+         div.style.background = bgColor
+         }
+       }, 2000)
+   
+    div.appendChild(title)
+    flexContainer.append(div)
 }
 
 
