ddc81dfa63a552dfa938fca380899d56b844ab88
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-01-17 11:37:44 +0200

    refactored

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bottomContainer = document.querySelector('.bottom-container')
 const generateButton = document.querySelector('.button-generate')
 const stopButton = document.querySelector('.button-stop')
 const input = document.querySelector('.input')
-        
+
+//returns random generated id
 const generateHexaColor = () => {
     let string = '0123456789abcdef'
     let hexaColor = '#'
@@ -15,6 +16,7 @@ const generateHexaColor = () => {
     return hexaColor
 }
 
+//running 
 const run = (j=5) => {
     for (let i = 0; i < j; i++) {
         const title = document.createElement('h2')
@@ -36,70 +38,59 @@ const run = (j=5) => {
             div.style.background = generatedColor;        
         }, 2000)
 
-        stopButton.addEventListener('click', () => {
-            clearInterval(interval);
-        })  
-
-        copyButton.addEventListener('click', () => {
-            let code = title.textContent;
-            let textArea = document.createElement('input');
-            textArea.value = code;
-            document.body.appendChild(textArea);
-            textArea.select();
-            document.execCommand('copy')
-            console.log(code)
-            textArea.remove();
-        })
-                
-        copyButton.addEventListener('mouseover', () => {
-            clearInterval(interval);
-        })
-                
-        copyButton.addEventListener('mouseout', () => {
-            bottomContainer.textContent = '';
-            run()
-                    
-        })
+        stop(interval);
+        copy(copyButton, title);
+        mouseOver(copyButton, interval);
+        mouseOut(copyButton);        
     }
 }
 
-// const errorMsg = () => {
-//      const errorMsg = document.querySelector('p');
-//      errorMsg.textContent = "The number must be greater than 5"
-//      errorMsg.style.color = "red"
-//      document.getElementById('div-ui').appendChild(errorMsg)
-//      run(5);
-
-// }
-        
+// USER ACTIONS (generate, stop, copy, mouse over, mouse out)
 generateButton.addEventListener('click', () => {    
     bottomContainer.textContent = '';
 
-    if (input.value == null) {
+    if (input.value == null || input.value < 5) {
         run()
-    } else if (input.value < 5 ) {
-        // alert("Must be a number greater than 5")
-        run()
+    // } else if ( ) {
+    //     // alert("Must be a number greater than 5")
+    //     run()
     } else {    
         run(input.value)
-    }
+    }  
     input.value = null;
-
-    // switch(inputValue) {
-    //     case inputValue < 5:
-    //         alert("The number must be greater than 5");
-    //         inputValue = null;
-    //         run(5);
-    //         break;
-    //     case inputValue > 5:
-    //         run(inputValue);
-    //         inputValue = null;
-    //         break;
-    //     default:
-    //         run(5) 
-    //         break;
-    // }
-        
 })
-               
+
+const stop = (interval) => {
+    stopButton.addEventListener('click', () => {
+        clearInterval(interval);
+    })  
+} 
+
+const copy = (copyButton, title) => {
+    copyButton.addEventListener('click', () => {
+        let code = title.textContent;
+        let textArea = document.createElement('input');
+        textArea.value = code;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy')
+        console.log(code)
+        textArea.remove();
+    })
+}
+
+const mouseOver = (copyButton, interval) => {
+    copyButton.addEventListener('mouseover', () => {
+        clearInterval(interval);
+    })
+}
+
+const mouseOut = (copyButton) => {
+    copyButton.addEventListener('mouseout', () => {
+        bottomContainer.textContent = '';
+        run()            
+    })
+}
+
+//runs initial session
 run();
